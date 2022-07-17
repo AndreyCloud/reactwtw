ecb164a82c711d83fb7fddcb9456a83856ef2dd4
AndreyCloud <d.cloud@mail.ru>
2022-07-17 21:57:43 +0300

    PreviewVideo

--- src/components/CardFilm/CardFilm.tsx
+++ src/components/CardFilm/CardFilm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Film } from '../../types/films';
 
 type CardFilmProps = {
@@ -6,15 +6,35 @@ type CardFilmProps = {
 }
 
 function CardFilm({film}: CardFilmProps): JSX.Element {
+
+  const [idPreview, setIdPreview] = useState<string>('');
+
+  const VideoPreview = (vlink: string) => setIdPreview(vlink);
+  const CardOut = () => setIdPreview('');
+
+  const vueCard = idPreview !== '' ?(
+    <video
+      muted
+      autoPlay
+      width={280}
+      height={175}
+      src= {idPreview}
+    />) : (
+    <img
+      src={film.preview_image}
+      alt={film.preview_image}
+      width="280"
+      height="175"
+    />);
+
   return (
-    <article className="small-film-card catalog__films-card">
+    <article
+      onMouseEnter={() => VideoPreview(film.preview_video_link)}
+      onMouseLeave={() => CardOut()}
+      className="small-film-card catalog__films-card"
+    >
       <div className="small-film-card__image">
-        <img
-          src={film.preview_image}
-          alt="Fantastic Beasts: The Crimes of Grindelwald"
-          width="280"
-          height="175"
-        />
+        {vueCard}
       </div>
       <h3 className="small-film-card__title">
         <a className="small-film-card__link" href="film-page.html">
